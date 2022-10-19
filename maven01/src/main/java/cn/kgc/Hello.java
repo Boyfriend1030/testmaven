6eaf10e7fe026cf0e64c354b325b66b61045b944
Boyfriend1030 <94731920+Boyfriend1030@users.noreply.github.com>
2022-10-19 15:35:40 +0800

    pull commit

--- maven01/src/main/java/cn/kgc/Hello.java
+++ maven01/src/main/java/cn/kgc/Hello.java
@@ -13,5 +13,6 @@ public class Hello {
         System.out.println("hello4");
         System.out.println("master test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
